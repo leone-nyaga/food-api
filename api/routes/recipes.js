d53ec6ef8575f71dc0d6224d4210a3cf28310e33
leone-nyaga <leonenyaga@gmail.com>
2024-12-30 00:21:10 +0300

    added endpoints

--- api/routes/recipes.js
+++ api/routes/recipes.js
@@ -16,4 +16,47 @@ const readFoodFile = () => {
   return JSON.parse(data);
 };
 
+/** synchronous function that writes data to the file.
+ * if file doesn't exit, it will be created
+ */
+const writeFoodFile = (data) => {
+  fs.writeFileSync(foodFilePath, JSON.stringify(data, null, 2));
+};
+
+/* get all */
+router.get('/', (req, res) => {
+  const recipes = readFoodFile();
+  res.json(recipes);
+});
+
+/* get recipe by id */
+router.get('/:id', (req, res) => {
+  const recipes = readFoodFile();
+  const recipe = recipes.find((r) => r.id === parseInt(req.params.id));
+  if (!recipe) {
+    return res.status(404).json({ error: 'Recipe not found' });
+  }
+  res.json(recipes);
+});
+
+/* get recipe by category or name */
+router.get('/search', (req, res) => {
+  const { name, category } = req.query;
+  const recipes = readFoodFile();
+  let filteredRecipes = recipes;
+
+  if (name) {
+    filteredRecipes = filteredRecipes.filter((r) => 
+      r.name.toLowerCase().includes(name.toLowerCase())
+    );
+  }
+
+  if (category) {
+    filteredRecipes = filteredRecipes.filter((r) => 
+      r.category.toLowerCase() === category.toLowerCase()
+    );
+  }
+
+  res.json(filteredRecipes);
+});
 
